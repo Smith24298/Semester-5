2a5627e6e19f54cc42de96517e595e66f185eee4
Smith24298 <dev.main.smith.2006@gmail.com>
2026-08-18 13:45:40 +0530

    Remove legacy search functionality and server code; add new index.html with Tailwind CSS integration

--- FSD/prac1/app.js
+++ FSD/prac1/app.js
@@ -6,12 +6,12 @@ const object = {
     "age" : 25
 }
 
-const jsonString = JSON.stringify(object,null,4);
-const path = 'FSD\\prac1\\data.json';
+const jsonString = JSON.stringify(object,null,200);
+const path = 'data.json';
 
 fs.writeFile(path,jsonString,(err)=>{
     if(err){
-        console.log("Error in writing file");
+        console.log("Error in writing file",err);
     }else{
         console.log("file write sucessfull");
     }
